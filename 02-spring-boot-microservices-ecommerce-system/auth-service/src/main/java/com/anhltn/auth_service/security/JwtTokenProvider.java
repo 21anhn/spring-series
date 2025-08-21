b357760271a1d8ff107ec429722026a1dcba3e55
21anhn <ltnanh21@gmail.com>
2025-08-21 22:47:22 +0700

    fix: Fix incorrect @Value annotation syntax for jwt.secret-key

--- 02-spring-boot-microservices-ecommerce-system/auth-service/src/main/java/com/anhltn/auth_service/security/JwtTokenProvider.java
+++ 02-spring-boot-microservices-ecommerce-system/auth-service/src/main/java/com/anhltn/auth_service/security/JwtTokenProvider.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider {
 
-    @Value("jwt.secret-key")
+    @Value("${jwt.secret-key}")
     private String secretKey;
     private final long accessTokenValidity = 1000 * 60 * 15; // 15 minutes
     private final long refreshTokenValidity = 1000 * 60 * 60 * 24 * 7; // 7 days
