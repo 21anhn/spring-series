5529f3abe1003eced4ce2f1a413154a07c916cab
21anhn <ltnanh21@gmail.com>
2025-08-21 09:36:04 +0700

    feat: Add global exception handler

--- 02-spring-boot-microservices-ecommerce-system/common/src/main/java/com/anhltn/common/exception/GlobalExceptionHandler.java
+++ 02-spring-boot-microservices-ecommerce-system/common/src/main/java/com/anhltn/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,60 @@
 package com.anhltn.common.exception;
 
+import com.anhltn.common.response.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException exception,
+                                                    HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                exception.getMessage(),
+                request.getRequestURI(),
+                false
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<?> handleUnauthorizedException(UnauthorizedException exception,
+                                                         HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                exception.getMessage(),
+                request.getRequestURI(),
+                false
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralException(Exception exception, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Unexpected Error",
+                exception.getMessage(),
+                request.getRequestURI(),
+                false
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
--- 02-spring-boot-microservices-ecommerce-system/common/src/main/java/com/anhltn/common/response/ErrorResponse.java
+++ 02-spring-boot-microservices-ecommerce-system/common/src/main/java/com/anhltn/common/response/ErrorResponse.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ApiResponse<T> {
+public class ErrorResponse {
+    private LocalDateTime timestamp;
     private int statusCode;
-    private boolean success;
+    private String error;
     private String message;
-    private T data;
+    private String path;
+    private boolean success;
 }
