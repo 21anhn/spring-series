68ea94f405f4895763e2e7513d31dc23b2e4d4e8
21anhn <ltnanh21@gmail.com>
2025-08-27 23:33:11 +0700

    chore: Init UserService interface, implementation

---
+++ 02-spring-boot-microservices-ecommerce-system/user-service/src/main/java/com/anhltn/user_service/service/UserService.java
@@ -0,0 +1,4 @@
+package com.anhltn.user_service.service;
+
+public interface UserService {
+}
---
+++ 02-spring-boot-microservices-ecommerce-system/user-service/src/main/java/com/anhltn/user_service/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.anhltn.user_service.service.impl;
+
+import com.anhltn.user_service.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
